03a021670f97446d9e2a29eb0fb9c7f7e2226f86
r1ckych <mcmrkr@gmail.com>
2021-12-03 10:04:47 +0800

    Refactor subject reader program to properly use functions

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,18 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_subject_details(data)
+
+
+def print_subject_details(data):
+    for detail in data:
+        print(f"{detail[0]} is taught by {detail[1]} and has {detail[2]} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    information = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +28,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        information.append(parts)
         print("----------")
     input_file.close()
+    return information
 
 
 main()
