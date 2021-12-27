32283ae8b15c933965b7a4cdebae96d558660017
r1ckych <mcmrkr@gmail.com>
2021-12-27 22:49:50 +0800

    add fuel by using add method

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -8,6 +8,7 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car(100)
     my_car.drive(30)
+    my_car.add_fuel(20)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
