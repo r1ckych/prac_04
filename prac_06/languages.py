085355a40a35cab5c55b7f16d9c3b58f02edecdc
r1ckych <mcmrkr@gmail.com>
2021-12-28 18:57:38 +0800

    Initial commit

---
+++ prac_06/languages.py
@@ -0,0 +1,8 @@
+
+from prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+print(python)
