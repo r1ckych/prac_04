e536151a9751d5cf0198bbd536cea22074965a44
r1ckych <mcmrkr@gmail.com>
2021-12-28 18:13:16 +0800

    Attempt to drive 115 km

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -7,8 +7,7 @@ from prac_06.car import Car
 def main():
     """Demo test code to show how to use car class."""
     my_car = Car(100)
-    my_car.drive(30)
-    my_car.add_fuel(20)
+    my_car.drive(115)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
