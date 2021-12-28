d40489ce1a3d5b912cddcd604c54961b30fd9ddd
r1ckych <mcmrkr@gmail.com>
2021-12-28 18:20:28 +0800

    print your car object

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -8,12 +8,6 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car(100)
     my_car.drive(115)
-    print("fuel =", my_car.fuel)
-    print("odo =", my_car.odometer)
     print(my_car)
-
-    print("{} {}, {}".format(my_car.new_car, my_car.fuel, my_car.odometer))
-    print("{self.new_car} {self.fuel}, {self.odometer}".format(self=my_car))
-
 
 main()
