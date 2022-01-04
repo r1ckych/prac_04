9c5e5638b70cfbed0c2ed81c4b18c51739d82c50
r1ckych <mcmrkr@gmail.com>
2022-01-04 20:54:44 +0800

    add increment and validation functions

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -18,7 +18,19 @@ class ConverterApp(App):
     def handle_calculate(self):
         value = self.get_valid_miles()
         result = value * MILES_TO_KM
-        self.root.ids.output_label.text = self.message
+        self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, change):
+        value = self.get_valid_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.handle_calculate()
+
+    def get_valid_miles(self):
+            try:
+                value = float(self.root.ids.input_miles.text)
+                return value
+            except ValueError:
+                return 0
 
 
 ConverterApp().run()
