17ca3450efb8b99996d8674f855ef9f43d571bc8
r1ckych <mcmrkr@gmail.com>
2022-01-04 20:47:17 +0800

    add build function

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -3,3 +3,17 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+MILES_TO_KM = 1.60934
+
+
+class ConverterApp(App):
+
+    message = StringProperty()
+
+    def build(self):
+         self.title = "Convert Miles to Kilometres"
+         self.root = Builder.load_file('convert_miles_km.kv')
+         return self.root
+
+
+ConverterApp().run()
