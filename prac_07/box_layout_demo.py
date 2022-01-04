35093f34758d0410cdcbc4945ca00a6a8d5371e0
r1ckych <mcmrkr@gmail.com>
2022-01-04 20:55:50 +0800

    Update greet and handler functions

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -8,5 +8,14 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("test")
+        self.root.ids.output_label.text = 'Hello ' + self.root.ids.input_name.text
+
+    def event_handler(self):
+        print('reset')
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = 'Greet'
+
 
 BoxLayoutDemo().run()
