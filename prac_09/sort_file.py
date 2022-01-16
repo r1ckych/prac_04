dec1191e3934b136add6860d397b0ca0e6ffe8c2
r1ckych <mcmrkr@gmail.com>
2022-01-16 22:44:39 +0800

    get filenames from directory

--- prac_09/sort_file.py
+++ prac_09/sort_file.py
@@ -13,4 +13,13 @@ def main():
     create_directories(extensions)
     move_files(extensions)
 
+
+def get_filenames():
+    filenames = []
+    for filename in os.listdir('.'):
+        if not os.path.isdir(filename):
+            filenames.append(filename)
+    return filenames
+
+
 main()
