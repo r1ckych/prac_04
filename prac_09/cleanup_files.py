95b0df5f0ccab8b231faffd82339d6e3afce37ec
r1ckych <mcmrkr@gmail.com>
2022-01-16 22:18:20 +0800

    Add rename files function

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -2,7 +2,6 @@
 CP1404/CP5632 Practical
 Demos of various os module examples
 """
-import shutil
 import os
 
 
@@ -13,10 +12,25 @@ def main():
     # Change to desired directory
     os.chdir('Lyrics/')
 
+    dir_list = os.listdir('.')
+
+    rename_files(dir_list)
+
+
+def rename_files(dir_list):
+    for dir in dir_list:
+        os.chdir(str(dir))
+        for filename in os.listdir('.'):
+            if not os.path.isdir(filename):
+                new_name = get_fixed_filename(filename)
+                os.rename(filename, new_name)
+        os.chdir('..')
+
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+
 
 
 main()
